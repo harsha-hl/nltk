00bdef397aab57e00557bab9c7a983de13ae91bb
Harsha H L <harshahl@Harshas-MacBook-Air.local>
2022-06-23 00:20:49 +0530

    Ananya genius

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
 from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, jsonify
+import json
 
 app = Flask(__name__)  
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html") 
+    objects = []    # a Python object (dict):
+    obj1 = {
+      "name": "burette",
+      "fill": 0,
+      "color": "nil",
+      "verb": "nil",
+      "action": "nil",
+      "pos": "under" 
+    }
+    obj2 = {
+      "name": "beaker",
+      "fill": 1,
+      "color": "blue",
+      "verb": "pour",
+      "action": "nil",
+      "pos": "default" 
+    }
+    objects.append(json.dumps(obj1))
+    objects.append(json.dumps(obj2))
+
+    return render_template("index.html", objs = objects) 
--- static/script.js
+++ static/script.js
@@ -11,6 +11,16 @@ function position(x,y,deg,path){
 }
 
 document.addEventListener("DOMContentLoaded", function(){
+    var objs = document.getElementById('myData').value;
+    objs = objs.slice(1, objs.length-1);
+    var o = [];
+    while (objs.length > 0){
+        o.push(JSON.parse(objs.slice(1,objs.indexOf('}') + 1)));
+        objs = objs.slice(objs.indexOf('}') + 4);
+    }
+    console.log(o);
+
+    // console.log(typeof(JSON.parse(objs[0])));
     position(200,250,00,"static/beaker_empty.png");
     position(500,250,45,"static/beaker_empty.png");
     position(900,250,90,"static/beaker_empty.png");
