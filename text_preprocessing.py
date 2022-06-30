79c898235f75356bf12232aef4bdaa54ebff5829
Ananya Rao <ananyarao610@gmail.com>
2022-06-30 11:22:06 +0530

    made changes

--- text_preprocessing.py
+++ text_preprocessing.py
@@ -11,6 +11,7 @@ from nltk.stem.porter import PorterStemmer
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk import pos_tag
 from nltk.probability import FreqDist
+from bs4 import BeautifulSoup
 
 def getObjects(line):
     #print(word_tokenize(text))
@@ -34,11 +35,16 @@ def getObjects(line):
 
     print("\n\nPOS tagged-\n")
     tagged = pos_tag(words)   #should have been lemmed not words
-    for i in tagged:
+    print(tagged)
+    '''for i in tagged:
         if i[0] in lemmed:
             print(i)
+    '''
 
-        
+    with open('data.xml', 'r') as f:
+        data = f.read()
+    bs = BeautifulSoup(data, "xml")
+
     position_words = ['under', 'over', 'above', 'below', 'near', 'towards']
     apparatus = ["beaker", "burette", "pipette", "bunsenburner", "burner", "roundbottomflask", "conicalflask", "tripodstand", "wiregauze","testtube"]
     objects =[]
@@ -53,7 +59,7 @@ def getObjects(line):
 
     for i in tagged:
         if i[1] == 'NN':
-            if i[0] in apparatus:
+            if bs.find('obj', {'name':i[0]}) != None:
                 objects.append(i[0])
                 if temp=="" and verb_temp!="" :
                     verbs[i[0]]=verb_temp;
@@ -62,22 +68,15 @@ def getObjects(line):
                 temp=i[0]
                 count+=1
         elif i[1]=='IN':
-            if i[0] in position_words:
-                if i[0] == 'under' or i[0]=='below':
-                    if temp!="":
-                        position[temp] = "down"
-                    else:
-                        position_temp="down"
-                elif i[0]=='above' or i[0]=='over':
-                    if temp!="":
-                        position[temp]="up"
-                    else:
-                        position_temp="up"
-            else:
-                if temp!= "":
-                    position[temp]=i[0]
+            p=bs.find('pos',{'name':i[0]})
+            print("P is:",i[0],p)
+            if p != None:
+                if temp!="":
+                    position[temp]= p.get('type')
+                    print("type-", p.get('type'))
+            
                 else:
-                    position_temp=i[0]
+                    position_temp=p.get('type')
         elif i[1][0]=='V':
             if temp!="":
                 verbs[temp]=i[0]
@@ -85,14 +84,17 @@ def getObjects(line):
                 verb_temp =i[0];
     
     x=[]
+    print("\n\n\nobj,pos,verb,count",objects,position,verbs,count)
 
+   
     for i in range(count):
         print("\nFor ",i,"th object")
         try:
             name=objects[i]
             print("Object is:" ,objects[i])
             fill=0
-            src="abc"
+            img=bs.find('img',{"name": objects[i]})
+            src= img.get('src')
             colour ="default"
             pos= position[objects[i]]
             print("Position is:",pos)
@@ -104,11 +106,11 @@ def getObjects(line):
 
         x.append({"name":name, "fill":fill,"src":src, "colour":colour,"verb":verb,"position":pos})
         pos="default";verb="default"
-
-    return x
         
-
+        print(x)
+    return x
     
+
 
 def main():         
     text = "Place stearic acid in a testtube boiling over a bunsen burner. "
@@ -116,9 +118,11 @@ def main():
     #print(text)
     sentence = sent_tokenize(text)
     print(sent_tokenize(text))
-
+    obj=[]
     for i in sentence:
         x=getObjects(i)
-        print("Final output-\n",x)
-
+        obj.append(x)
+    print("Final output-\n",obj)
+    return obj
+        
 main()
