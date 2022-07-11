102d2ec942c6aaea05e9c23315b66bccd9c52ee1
Ananya Rao <ananyarao610@gmail.com>
2022-07-12 00:06:21 +0530

    New experimentt

--- text_preprocessing.py
+++ text_preprocessing.py
@@ -12,9 +12,10 @@ from nltk import pos_tag
 from nltk.probability import FreqDist
 from bs4 import BeautifulSoup
 
+
 a = []
-global text
-text = '''Pour dilute HCl from a beaker into a test tube containing salt solution.
+global text, text2
+text2 = '''Pour dilute HCl from a beaker into a test tube containing salt solution.
     No white precipitate formed in test tube indicating absence of Pb2+. 
     Pour H2S from beaker to test tube. 
     Black precipitate is formed in test tube indicating presence of Cu2+ or Pb2+.
@@ -24,8 +25,31 @@ text = '''Pour dilute HCl from a beaker into a test tube containing salt solutio
     Solution in test tube turns blue confirming presence of Cu2+.
     Pour K4[Fe(CN)6] solution from conicalflask to test tube containing second part. 
     Chocolate brown precipitate of Copper ferrocyanide is formed in test tube confirming the presence of Cu2+ ions.
-
     '''
+text = '''
+ Add a few drops of dilute H2SO4 (sulfuric acid) from conical flask to a small quantity of the salt in a test tube.
+Light brown gas with a pungent smell is evolved from the test tube.
+The anion maybe Nitrate.
+Add Iron(II) Sulphate from beaker to the test tube containing salt solution.
+Add acid (H2SO4) from the container to the corners of the test tube
+A Brown ring is formed at the junction of the acid and the solution in the test tube.
+Nitrate is confirmed
+
+Add a few drops of Hcl from container to the test tube containing salt solution.
+No change observed.
+To the above solution in test tube, pass H2S gas.
+No changes observed.
+Add solid NH4Cl to the test tube containing salt solution and then add excess NH4OH from conical flask.
+Add ammonium carbonate, ammonium chloride, and ammonium hydroxide to the original solution in the test tube.
+White precipitate is obtained in the test tube.
+Cation maybe Barium, Strontium or Calcium
+Add K2Cr2O4 from conical flask to test tube containing salt solution. A yellow precipitate is formed.
+CAtion maybe Ba2+
+Make a paste of the salt by mixing it in a petri dish. Add a few drops of concentrated hydrochloric acid. Now skim off some of the paste with a glass rod and expose it to a Bunsen Burner’s flame.
+Green coloured flame confirms Ba2+ as cation.
+
+'''
+
 
 
 
@@ -65,6 +89,7 @@ def getObjects(line):
     colour_temp=""
     verb="default";posx=""; flag=0;posy=""
     colour="#cccccc"
+    check_verb = ["add", "pour"]
 
 
     for i in tagged:
@@ -157,10 +182,10 @@ def getObjects(line):
         if posx == "" or posy=="":
             posx= "700"
             posy= "-600"
-        if verb=='pour':
+        if verb in check_verb:
             src="static/"+name+"_pour.png"
-            posx="510"        #
-            posy="-425"       #
+            posx="510"        
+            posy="-425"       
         if name=='precipitate':
             posx="706"
             posy="-593"
